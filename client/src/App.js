67eb4576adbd73fefc617bb96f2608ec6a169a62
Tanmay Khandelwal <iamtanmaykh07@gmail.com>
2023-08-26 23:29:01 +0530

    client part navbar added

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter,Navigate,Routes,Route } from "react-router-dom";
+import Home from "scenes/homepage";
+import Login from "scenes/loginpage";
+import Profile from "scenes/profilepage";
+import { CssBaseline,ThemeProvider, } from "@mui/material";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import {createTheme} from '@mui/material/styles';
+import { themeSettings } from "./theme.js";
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const mode = useSelector((state) => state.mode);
+    const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <ThemeProvider theme={theme}>
+                    <CssBaseline/>
+                    <Routes>
+                        <Route path="/" element={<Login/>}/>
+                        <Route path="/home" element={<Home/>}/>
+                        <Route path="/profile/:userId" element={<Profile/>}/>
+                    </Routes>
+                </ThemeProvider>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
